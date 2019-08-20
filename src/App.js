3fb6187de82fd53754787690d5f273fa9c132705
sjs_2215 <soojinsarah@naver.com>
2019-08-20 20:05:36 +0900

    using array.from <immutable>

--- src/App.js
+++ src/App.js
@@ -47,12 +47,14 @@ class App extends Component {
       _article = <CreateContent onSubmit={function(_title,_desc){
         this.max_content_id=this.max_content_id+1;
 
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        )
+        // var _contents = this.state.contents.concat(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // )
 
+        var newContents = Array.from(this.state.contents);
+        newContents.push({id:this.max_content_id, title:_title, desc:_desc});
         this.setState({
-          contents:_contents
+          contents:newContents
         });
       }.bind(this)}></CreateContent>
     }
