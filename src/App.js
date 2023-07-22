4917e928ce243a207ffa3488b0de3ab95aee2304
sjs2215 <soojinsarah@naver.com>
2023-07-22 20:49:10 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import './App.css'
+function Counter(){ // 컴포넌트를 정의하는 함수
+//return 은 하나의 태그만 해야함. 그래서 div로 감싸줌.
+
+return <div>
+<h1>Counter</h1>
+<button>+</button> 
+</div>
+}
 
 function App() {
   return (
     <div>
-      hi
+      {/* 아래와 같이 컴포넌트 사용 */}
+      <Counter></Counter>  
     </div>
   );
 }
