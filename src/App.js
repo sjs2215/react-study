7a60236f11c9ea44d26b9648f15e99508abea2a5
sjs2215 <soojinsarah@naver.com>
2023-07-22 21:16:19 +0900

    props추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-function Counter(){ // 컴포넌트를 정의하는 함수
+function Counter(props){ // 컴포넌트를 정의하는 함수
 //return 은 하나의 태그만 해야함. 그래서 div로 감싸줌.
-console.log(props);
+console.log(props.title);
 return <div>
-<h1>Counter</h1>
-<button>+</button> 0
+<h1>{props.title}</h1>
+<button>+</button> {props.initValue}
 </div>
 }
 
@@ -12,8 +12,8 @@ function App() {
   return (
     <div>
       {/* 아래와 같이 컴포넌트 사용 */}
-      <Counter title="불면증 카운터"></Counter>  
-
+      <Counter title="불면증 카운터" initValue="10"></Counter>  
+      <Counter title="불면증 카운터" initValue={10}></Counter>  
     </div>
   );
 }
