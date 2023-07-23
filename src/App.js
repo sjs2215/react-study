60d6a4d69f0006c62e3844df691475dc9592ee07
sjs2215 <soojinsarah@naver.com>
2023-07-23 20:15:08 +0900

    사용자의 입력을 받는 input 컨트롤에서는 state를 만들어서 onChange로 값의 변화를 연결한다.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import {useState} from 'react';
 function Counter({title, initValue}){ // 컴포넌트를 정의하는 함수
 //return 은 하나의 태그만 해야함. 그래서 div로 감싸줌.
   const [count, setCount] = useState(initValue);
+  const [step, setStep] = useState(1);
   function up(){
-    setCount(count+1);
+    setCount(count+step);
   }
 return <div>
   <h1>{title}</h1>
-  <button onClick={up}>+</button> {count}
+  <button onClick={up}>+</button> 
+  <input type="number" value={step} onChange={(evt)=>{
+  setStep(Number(evt.target.value));
+  }}/>
+  {count}
 </div>
 }
 
