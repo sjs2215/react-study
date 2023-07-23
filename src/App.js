b85fa390c502e6b6872c14823566bec11b0a8c76
sjs2215 <soojinsarah@naver.com>
2023-07-23 22:18:37 +0900

    mui 설치 container를 이용하여 화면 가운데 정렬

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import {useEffect, useState} from 'react';
+import { Container } from '@mui/system';
 
 function Counter({title, initValue}){ // 컴포넌트를 정의하는 함수
 //return 은 하나의 태그만 해야함. 그래서 div로 감싸줌.
@@ -53,11 +54,11 @@ function CounterUseEffect(){
 
 function App() {
   return (
-    <div>
+    <Container>
       {/* 아래와 같이 컴포넌트 사용 */}
       <Counter title="불면증 카운터" initValue={10}></Counter>  
       <CounterUseEffect></CounterUseEffect>
-    </div>
+    </Container>
   );
 }
 
