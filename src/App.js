2bc074036020fc0ca45af8c3dcc444888a036493
sjs2215 <soojinsarah@naver.com>
2023-07-23 21:54:58 +0900

    useeffect, closure, set함수 return 값, unmount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function Counter({title, initValue}){ // 컴포넌트를 정의하는 함수
 //return 은 하나의 태그만 해야함. 그래서 div로 감싸줌.
@@ -29,12 +29,30 @@ return <div>
 </div> 
 }
 
+function CounterUseEffect(){
+  const [count, setCount] = useState(0);
+  console.log('counteruseeffect',count);
+  useEffect(()=>{
+    const id = setInterval(()=>{
+      setCount(oldCount=>oldCount+1);
+    },1000)
+    return ()=>{
+      console.log('clean');
+      clearInterval(id);
+    }
+  },[]);
+ 
+  return <div>
+    <h1>useEffect Counter</h1> {count}
+  </div>
+}
+
 function App() {
   return (
     <div>
       {/* 아래와 같이 컴포넌트 사용 */}
       <Counter title="불면증 카운터" initValue={10}></Counter>  
-
+      <CounterUseEffect></CounterUseEffect>
     </div>
   );
 }
