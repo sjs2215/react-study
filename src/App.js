02c86f9c112853ee11376280d89b52a32fd536ed
sjs2215 <soojinsarah@naver.com>
2023-07-23 21:11:15 +0900

    immutability 상태의 값이 배열,객체와 같은 값의 컨테이너인 경우, 상태를 복제한 후에 데이터를 추가,수정,삭제 해야한다. 그렇게 해야 react는 이전상태/이후 상태가 변경된 상태라는 것을 인지한다. = immutability

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ function Counter({title, initValue}){ // 컴포넌트를 정의하는 함수
 //return 은 하나의 태그만 해야함. 그래서 div로 감싸줌.
   const [count, setCount] = useState(initValue);
   const [step, setStep] = useState(1);
+  const [history, setHistory] = useState([5,5]);
+
   function up(){
-    setCount(count+step);
+    const newCount = count+step;
+    const newHistory = [...history];
+    setCount(newCount);
+    newHistory.push(newCount);
+    setHistory(newHistory);
   }
+  const stepHandler = (evt) => {
+    setStep(Number(evt.target.value));
+  };
 return <div>
   <h1>{title}</h1>
   <button onClick={up}>+</button> 
-  <input type="number" value={step} onChange={(evt)=>{
-  setStep(Number(evt.target.value));
-  }}/>
+  {/* react에선 for문 보다 map함수를 더 많이 쓴다. 응집력이 더 높음 */}
+  <input type="number" value={step} onChange={stepHandler}/>
   {count}
+  <ol>
+    {history.map((e, index)=><li key={index}>{e}</li>)}
+  </ol>
 </div>
 }
 
